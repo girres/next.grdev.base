cc5008195b7f9a82ca11db932c86c4542953dc81
Harold GR <harold@girres.com.co>
2023-08-05 14:05:08 -0500

    Prettier & EsLint for NextJS

--- src/components/ButtonAlert.js
+++ src/components/ButtonAlert.js
@@ -1,19 +1,19 @@
-"use client";
+'use client';
 
-import React, { useContext } from "react";
-import PropTypes from "prop-types";
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
 
-import { Context } from "@/context/index";
+import { Context } from '@/context/index';
 
 function Component({ title = null }) {
   const { Alerts } = useContext(Context);
 
   return (
     <button
-      onClick={() => Alerts.Info("HI World!!")}
-      className="btn btn-primary"
+      onClick={() => Alerts.Info('HI World!!')}
+      className='btn btn-primary'
     >
-      {title || "Show Alert"}
+      {title || 'Show Alert'}
     </button>
   );
 }
